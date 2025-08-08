5166a1c6596e16b944b1bd2101f5a5b8e7d49408
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-08 15:27:56 +0300

    chore: use formater and linter to ensure project clearability in code

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css'
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
 })
-export class Login {
-
-}
+export class Login {}
