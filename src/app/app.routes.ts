e4c6319d9bb7a4f948738e656c1a0a358d69f03a
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-08 16:17:20 +0300

    chore: init error http handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { Http } from './shared/http/http';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: '**', component: Http },
+];
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class Login {}
+export class LoginComponent {}
