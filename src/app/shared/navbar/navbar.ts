e9fb0a2af7322ad5e6e673e1342a788ff8a6a9ad
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-12 14:06:02 +0300

    chore: use actual conditionning rendering

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,9 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
