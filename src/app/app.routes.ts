312683d17b6b4f1cfcb22b0b2861a1871739fc95
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-12 12:05:22 +0300

    feat: navbar for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { ErrorHttpComponent } from './shared/error/error.component';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '', component: Home },
   { path: 'auth/login', component: LoginComponent },
   { path: '**', component: ErrorHttpComponent },
 ];
