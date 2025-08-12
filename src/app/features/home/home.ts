a74af41bf2d37b5c3789cd632b00cacf5844de5f
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-12 19:39:31 +0300

    feat: home page with a hero section

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Navbar } from '../../shared/navbar/navbar';
 
 @Component({
@@ -7,4 +7,30 @@ import { Navbar } from '../../shared/navbar/navbar';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+  phrases: string[] = ['Story', 'Moment', 'Movie', 'Love'];
+  currentPhrase: string = this.phrases[0];
+  intervalId?: any;
+  fade = false;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    let index = 0;
+    this.intervalId = setInterval(() => {
+      this.fade = true;
+      setTimeout(() => {
+        index = (index + 1) % this.phrases.length;
+        this.currentPhrase = this.phrases[index];
+        this.fade = false;
+        this.cd.detectChanges();
+      }, 500);
+    }, 3000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+}
