725bfcce412e7c91ea963f62dfd708210a88b75c
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-13 11:59:20 +0300

    fix: home service test, and import mock http client

--- src/app/features/home/components/home.spec.ts
+++ src/app/features/home/components/home.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('Home', () => {
   let component: HomeComponent;
@@ -10,7 +12,11 @@ describe('Home', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideZonelessChangeDetection()],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
--- src/app/features/home/services/home.service.spec.ts
+++ src/app/features/home/services/home.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HomeService } from './home.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideZonelessChangeDetection(),
+      ],
+    });
     service = TestBed.inject(HomeService);
   });
 
