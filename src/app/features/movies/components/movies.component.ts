986b4a948f6d3698676f250650103df21b69caee
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-16 10:39:36 +0300

    feat: template for movies list

--- src/app/features/movies/components/movies.component.ts
+++ src/app/features/movies/components/movies.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Navbar } from '../../../shared/navbar/navbar';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-component',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css',
 })
-export class MoviesComponent {}
+export class MoviesComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+  searchQuery = '';
+
+  movies = [
+    {
+      title: 'Inception',
+      description:
+        'A skilled thief is given a chance at redemption if he can successfully perform inception.',
+      image: 'https://via.placeholder.com/120x160',
+    },
+    {
+      title: 'Interstellar',
+      description: 'A team of explorers travel through a wormhole in space.',
+      image: 'https://via.placeholder.com/120x160',
+    },
+    {
+      title: 'The Matrix',
+      description:
+        'A computer hacker learns about the true nature of his reality.',
+      image: 'https://via.placeholder.com/120x160',
+    },
+  ];
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.searchQuery = params['search'] || '';
+    });
+  }
+
+  get filteredMovies() {
+    if (!this.searchQuery) return this.movies;
+    return this.movies.filter((movie) =>
+      movie.title.toLowerCase().includes(this.searchQuery.toLowerCase()),
+    );
+  }
+}
--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  constructor(private router: Router) {}
+
+  searchValue: string = '';
   menuOpen = false;
+  @Output() searchEvent = new EventEmitter<string>();
+
+  onSearch() {
+    if (this.searchValue.trim()) {
+      this.router.navigate(['/movies'], {
+        queryParams: { search: this.searchValue },
+      });
+    } else {
+      this.router.navigate(['/movies']);
+    }
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
