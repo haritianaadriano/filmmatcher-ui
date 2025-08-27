1c867dae63a45d5eec815621f1cde5fda31a9510
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-08-27 12:07:28 +0300

    feat: go back and register form style

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Login } from '../../../types/login.type';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
