6431132c3e1f8af8f0dee52f0a1e012738ae4512
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-09-07 18:59:03 +0300

    feat: add a link to register directly if you do not have an account on login

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -3,11 +3,11 @@ import { Login } from '../../../types/login.type';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
