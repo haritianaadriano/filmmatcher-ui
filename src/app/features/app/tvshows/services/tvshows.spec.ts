d791b0dcf02b8aa92e74f188b0bfa9707cabe24d
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-10-02 19:44:44 +0300

    infra: disable auto deployment on unspecified branch

--- src/app/features/app/tvshows/services/tvshows.spec.ts
+++ src/app/features/app/tvshows/services/tvshows.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TvshowsService } from './tvshows.service';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('Tvshows', () => {
   let service: TvshowsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
     service = TestBed.inject(TvshowsService);
   });
 
