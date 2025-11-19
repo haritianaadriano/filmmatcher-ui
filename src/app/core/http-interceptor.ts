ce766880281164ccc796df63d5aa210f8ad73658
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-11-19 19:56:13 +0300

    chore: handle forbidden error on http interceptor

--- src/app/core/http-interceptor.ts
+++ src/app/core/http-interceptor.ts
@@ -1,17 +1,54 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { AuthService } from '../features/auth/services/auth.service';
+import { Authenticated } from '../types/authenticated.type';
+import { SKIP_AUTH } from './http-context';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const authToken = localStorage.getItem('auth_token');
+  const authService = inject(AuthService);
+  const accessToken = authService.getToken(); // your method or localStorage
+  let request = req;
 
-  if (!authToken) {
+  if (req.context.get(SKIP_AUTH)) {
     return next(req);
   }
 
-  const authReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${authToken}`,
-    },
-  });
+  if (accessToken) {
+    request = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  }
 
-  return next(authReq);
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      //
+      // handle access token expired
+      //
+      if (
+        (error.status === 401 || error.status === 403) &&
+        !request.url.includes('/auth/refresh')
+      ) {
+        return authService.refreshToken().pipe(
+          switchMap((newToken: Authenticated) => {
+            // Save new token
+            authService.setToken(newToken.token, newToken.email);
+
+            // Retry the original request with the new token
+            const retryReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${newToken.token}`,
+              },
+            });
+
+            return next(retryReq);
+          }),
+        );
+      }
+
+      return throwError(() => error);
+    }),
+  );
 };
