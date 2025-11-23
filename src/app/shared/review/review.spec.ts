fe87c6613ee44df92098a751fa22183e70d4c041
Haritiana Adriano <hei.adriano.4@gmail.com>
2025-11-23 17:24:16 +0300

    fix: review test and import zoneless providers

--- src/app/shared/review/review.spec.ts
+++ src/app/shared/review/review.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Review } from './components/review';
+import { Review } from './review';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('Review', () => {
   let component: Review;
@@ -8,6 +9,7 @@ describe('Review', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
       imports: [Review],
     }).compileComponents();
 
--- src/app/shared/review/service/review.spec.ts
+++ src/app/shared/review/service/review.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReviewService } from './review.service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('Review', () => {
   let service: ReviewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideZonelessChangeDetection()],
+    });
     service = TestBed.inject(ReviewService);
   });
 
