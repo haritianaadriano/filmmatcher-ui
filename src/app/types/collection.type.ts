66191adf76f16dee6675e0571bf3ac5da3fd6c46
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2025-12-28 19:26:27 +0300

    feat: user can create collection via ui
    * feat: collection service to create collection
    
    * feat: user can now create collection
    
    * fix: test doesn't pass from collection service import

--- src/app/types/collection.type.ts
+++ src/app/types/collection.type.ts
@@ -6,3 +6,27 @@ export interface CollectionApi {
   genre: string;
   posters: string[];
 }
+
+export class CreateCollection {
+  name: string | null = null;
+  id: string | null = null;
+  creation_datetime: Date | null = null;
+  updated_at: Date | null = null;
+  movies_id: string[] = [];
+  user_id: string | null = null;
+  description: string | null = null;
+  genre: string | null = null;
+
+  constructor() {}
+}
+
+export interface Collection {
+  name: string | null;
+  id: string | null;
+  creation_datetime: Date | null;
+  updated_at: Date | null;
+  movies_id: string[];
+  user_id: string | null;
+  description: string | null;
+  genre: string | null;
+}
