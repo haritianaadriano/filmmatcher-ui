c4471ae1a24f9f27c7379e20f1bec7635c3100af
Haritiana Adriano <hei.adriano.4@gmail.com>
2026-01-15 20:14:14 +0300

    feat: collection movies component

---
+++ src/app/features/app/collections/movies/collection-movies.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'collection-movies',
+  imports: [],
+  templateUrl: './movies.html',
+  styleUrl: './movies.css',
+})
+export class CollectionMoviesComponent {}
