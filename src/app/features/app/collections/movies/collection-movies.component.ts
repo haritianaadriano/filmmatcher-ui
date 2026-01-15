f69c7e611b6a987a628e1c0b0e36d15aa4ef1f83
Haritiana Adriano <hei.adriano.4@gmail.com>
2026-01-15 20:16:26 +0300

    chore: rename component file for common readability

--- src/app/features/app/collections/movies/collection-movies.component.ts
+++ src/app/features/app/collections/movies/collection-movies.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'collection-movies',
   imports: [],
-  templateUrl: './movies.html',
+  templateUrl: './collection-movies.component.html',
   styleUrl: './movies.css',
 })
 export class CollectionMoviesComponent {}
