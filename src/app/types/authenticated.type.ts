23bbea2ee4fc4a70e36296fa607a72da6f3b95df
Haritiana Adriano <hei.adriano.4@gmail.com>
2026-01-19 21:10:40 +0300

    refactor: use whoami to get connected user

--- src/app/types/authenticated.type.ts
+++ src/app/types/authenticated.type.ts
@@ -1,4 +1,8 @@
-export interface Authenticated {
-  email: string;
-  token: string;
+export interface RefreshTokenResponse {
+  token_type: 'Bearer';
+  issued_at: string;
+  expires_at: string;
+  access_expires_in: number;
+  access_token: string;
+  id_token?: string;
 }
