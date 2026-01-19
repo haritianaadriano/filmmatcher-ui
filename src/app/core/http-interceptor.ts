9954a2ad9d01f9dba25cebfeb8e518f8c870da40
Haritiana Adriano <hei.adriano.4@gmail.com>
2026-01-19 21:09:52 +0300

    refactor: http interceptor to handle refresh token

--- src/app/core/http-interceptor.ts
+++ src/app/core/http-interceptor.ts
@@ -1,23 +1,23 @@
 import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, switchMap, throwError } from 'rxjs';
+
 import { AuthService } from '../features/auth/services/auth.service';
-import { Authenticated } from '../types/authenticated.type';
+import { RefreshTokenResponse } from '../types/authenticated.type';
 import { SKIP_AUTH, SKIP_REFRESH } from './http-context';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const accessToken = authService.getToken(); // your method or localStorage
+  const accessToken = authService.getToken();
+
   let request = req;
 
-  if (req.context.get(SKIP_REFRESH)) {
+  // skip explicit
+  if (req.context.get(SKIP_AUTH) || req.context.get(SKIP_REFRESH)) {
     return next(req);
   }
 
-  if (req.context.get(SKIP_AUTH)) {
-    return next(req);
-  }
-
+  // attach access token
   if (accessToken) {
     request = req.clone({
       setHeaders: {
@@ -28,23 +28,26 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(request).pipe(
     catchError((error: HttpErrorResponse) => {
-      //
-      // handle access token expired
-      //
+      // access token expired
       if (error.status === 401 || error.status === 403) {
-        // Prevent multiple refresh loops
+        // prevent infinite refresh loop
         if (req.headers.get('X-Refresh-Attempt')) {
-          authService.logout(); // clear tokens & navigate
+          authService.logout();
           return throwError(() => error);
         }
 
         return authService.refreshToken().pipe(
-          switchMap((newToken: Authenticated) => {
-            authService.setToken(newToken.token, newToken.email);
+          switchMap((_response: RefreshTokenResponse) => {
+            const newAccessToken = authService.getToken();
+
+            if (!newAccessToken) {
+              authService.logout();
+              return throwError(() => error);
+            }
 
             const retryReq = req.clone({
               setHeaders: {
-                Authorization: `Bearer ${newToken.token}`,
+                Authorization: `Bearer ${newAccessToken}`,
                 'X-Refresh-Attempt': 'true',
               },
             });
