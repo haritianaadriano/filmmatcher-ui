f652338a3de303bdea2f6bfc3b8a98d812223166
NyAinaMickael <ermiteandrianarivelo@gmail.com>
2026-02-24 10:32:59 +0300

    refactor:creation an independent component to display a movie card and avoid code duplication

---
+++ src/app/features/app/movies/card/movie-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { NgIf, NgFor, NgClass, SlicePipe } from "@angular/common";
+import { MovieApi } from "../../../../types/movies-api.type";
+
+@Component({
+  selector: 'app-movie-card',
+  standalone: true,
+  imports: [NgIf, NgFor, NgClass, SlicePipe],
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.css'],
+})
+export class MovieCard {
+  @Input() movie: MovieApi | null = null;
+  @Output() isSelected = new EventEmitter<MovieApi>();
+}
